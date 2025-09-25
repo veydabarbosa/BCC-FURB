f2f1c0b0e60b1b6cebb4c278745fc1622e751916
Veyda Cristina Barbosa <veydaccbarbosa@gmail.com>
2025-09-25 08:47:23 -0300

    Update 6 - Consumo de Combustível.java
    add ip

--- 1º SEMESTRE/Introdução à Programação/Lista 1 - Pseudocódigos/6 - Consumo de Combustível.java
+++ 1º SEMESTRE/Introdução à Programação/Lista 1 - Pseudocódigos/6 - Consumo de Combustível.java
@@ -1,6 +1,6 @@
-#6. Desenvolva um algoritmo em pseudocódigo que represente um Veículo e possua os atributos modelo,
-capacidade do tanque e quilometragem rodada na última viagem. Implemente um método que calcule e
-exiba a média de consumo do veículo, considerando que o motorista encheu o tanque antes da viagem.
+//6. Desenvolva um algoritmo em pseudocódigo que represente um Veículo e possua os atributos modelo,
+//capacidade do tanque e quilometragem rodada na última viagem. Implemente um método que calcule e
+//exiba a média de consumo do veículo, considerando que o motorista encheu o tanque antes da viagem.
 
 inicioCarro
 variaveis: modelo, capacidadeTanque, kmRodado
